b6e2b33e71289e63d7f23031a6f5e235f5c11452
D69ion <dmitriy.shtele@gmail.com>
2020-10-30 01:48:12 +0400

    bug fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,40 +4,49 @@ import util.Parser.HTMLParser;
 import util.Validator.FilePathValidator;
 import util.Validator.URLValidator;
 
-import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     private final static Logger LOGGER = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                System.out.println("Paste the link");
-                String url = scanner.nextLine();
-
-                if (URLValidator.isValid(url)) {
-                    HTMLParser parser = new HTMLParser(HTMLDownloader.getDocumentFromURL(url));
-                    HashMap<String, Integer> uniqueWords = parser.parseTags();
-                    System.out.println("List of unique words:\r\n");
-                    System.out.println(uniqueWords.toString());
-                } else if(FilePathValidator.isValid(url)){
-                    HTMLParser parser = new HTMLParser(HTMLDownloader.getDocumentFromFile(url));
-                    HashMap<String, Integer> uniqueWords = parser.parseTags();
-                    System.out.println("List of unique words:\r\n");
-                    System.out.println(uniqueWords.toString());
-                } else {
-                    System.out.println("Invalid link or file path");
+        while (true) {
+            try(Scanner scanner = new Scanner(System.in)){
+                while (true) {
+                    System.out.println("Paste the link");
+                    String url = scanner.nextLine();
+                    if(url.equals("!quit")){
+                        return;
+                    }
+                    if (URLValidator.isValid(url)) {
+                        HTMLParser parser = new HTMLParser(HTMLDownloader.getDocumentFromURL(url));
+                        getUniqueWords(parser);
+                    } else if (FilePathValidator.isValid(url)) {
+                        HTMLParser parser = new HTMLParser(HTMLDownloader.getDocumentFromFile(url));
+                        getUniqueWords(parser);
+                    } else {
+                        System.out.println("Invalid link or file path");
+                        LOGGER.warn("Invalid link: " + url);
+                    }
                 }
+            } catch (NullPointerException e) {
+                System.out.println("Failed to load html page");
+                LOGGER.error("Failed to load html page", e);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                LOGGER.error(e.getMessage(), e);
             }
-        } catch (NullPointerException e) {
-            System.out.println("Failed to load html page");
-            LOGGER.error("Failed to load html page", e);
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
         }
+    }
 
+    private static void getUniqueWords(HTMLParser parser){
+        HashMap<String, Integer> uniqueWords = parser.parseTags();
+        System.out.println("List of unique words:\r\n");
+        for (Map.Entry<String, Integer> word: uniqueWords.entrySet()) {
+            System.out.println(word.getKey() + " - " + word.getValue());
+        }
     }
 
 }
--- src/main/java/util/Validator/FilePathValidator.java
+++ src/main/java/util/Validator/FilePathValidator.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FilePathValidator {
-    private static final String FILE_PATH_Pattern = "([a-zA-Z]:(\\\\w+)*\\\\[a-zA-Z0_9]+)?.html";
+    private static final String FILE_PATH_Pattern = "^(([a-zA-Z]:)|(\\\\{2}\\w+)\\$?)(\\\\(\\w[\\w].*))+(.html)$";
 
     public static boolean isValid(String checkedURL){
         Pattern pattern = Pattern.compile(FILE_PATH_Pattern);
